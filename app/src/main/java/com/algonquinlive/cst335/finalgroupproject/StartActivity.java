98489843025a279dcfcb2b59e68a949d1469f86b
Aaron Cox <cox00057@algonquinlive.com>
2018-03-02 11:49:47 -0500

    added new Bus activity, some roughing out

--- app/src/main/java/com/algonquinlive/cst335/finalgroupproject/StartActivity.java
+++ app/src/main/java/com/algonquinlive/cst335/finalgroupproject/StartActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.algonquinlive.cst335.finalgroupproject.octranspo.OCTranspoActivity;
+
 public class StartActivity extends Activity {
 
     private Button studentButton1;
@@ -43,8 +45,8 @@ public class StartActivity extends Activity {
         });
 
         studentButton4.setOnClickListener((View e) ->{
-            //Intent intent = new Intent(this, _);
-            //startActivity(intent);
+            Intent intent = new Intent(this, OCTranspoActivity.class);
+            startActivity(intent);
         });
     }
 }
---
+++ app/src/main/java/com/algonquinlive/cst335/finalgroupproject/octranspo/OCTranspoActivity.java
@@ -0,0 +1,17 @@
+package com.algonquinlive.cst335.finalgroupproject.octranspo;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.algonquinlive.cst335.finalgroupproject.R;
+
+public class OCTranspoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_octranspo);
+
+    }
+}
