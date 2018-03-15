637fb6de27760aa3d8fab1c604acf50f9d59cd1b
giang <vutruonggiang1910gmail.com>
2018-03-15 19:10:51 -0400

    giang branch initial commit

---
+++ app/src/main/java/com/algonquinlive/cst335/finalgroupproject/MccMainActivity.java
@@ -0,0 +1,13 @@
+package com.algonquinlive.cst335.finalgroupproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MccMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mcc_main);
+    }
+}
